ba6c60e0b09fcf49ecfb4b67e7ac9e63a9acc2c7
vulong <vulongkm1998@gmail.com>
2019-12-31 13:23:29 +0700

    Test ssh, learn edit file with VI

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ require("./startup/routes")(app);
 
 // ****************** START SERVER =================
 const port = process.env.PORT || 3009;
+
 // start server
 app.listen(port, ()=>{console.log(`Server starting at ${port}`)});
 
